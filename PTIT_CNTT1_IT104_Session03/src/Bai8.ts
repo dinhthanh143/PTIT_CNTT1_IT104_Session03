13664543a6cbf7a5b36594bc59a9f2db9e2e602d
dinhthanh143 <neonlimea@gmail.com>
2025-08-06 22:22:26 +0700

    Update Bai8.ts

--- PTIT_CNTT1_IT104_Session03/src/Bai8.ts
+++ PTIT_CNTT1_IT104_Session03/src/Bai8.ts
@@ -1,16 +1,49 @@
-function operate(a: number | string, b: number | string): void {
+function add(a: number | string, b: number | string): void {
     const numA = Number(a);
     const numB = Number(b);
-
     if (isNaN(numA) || isNaN(numB)) {
-        console.log("invalid input");
+        console.log("error");
         return;
     }
-
-    console.log(`Tong: ${numA + numB}`);
-    console.log(`Hieu: ${numA - numB}`);
-    console.log(`Tich: ${numA * numB}`);
-    console.log(`Thuong: ${numB !== 0 ? numA / numB : "Khong chia dc cho 0"}`);
+    console.log(numA + numB);
 }
 
-operate("10x",3)
+function subtract(a: number | string, b: number | string): void {
+    const numA = Number(a);
+    const numB = Number(b);
+    if (isNaN(numA) || isNaN(numB)) {
+        console.log("error");
+        return;
+    }
+    console.log(numA - numB);
+}
+
+function multiply(a: number | string, b: number | string): void {
+    const numA = Number(a);
+    const numB = Number(b);
+    if (isNaN(numA) || isNaN(numB)) {
+                console.log("error");
+
+        return;
+    }
+    console.log(numA * numB);
+}
+
+function divide(a: number | string, b: number | string): void {
+    const numA = Number(a);
+    const numB = Number(b);
+    if (isNaN(numA) || isNaN(numB)) {
+        console.log("error");
+        return;
+    }
+    if (numB === 0) {
+        console.log("ko chia dc cho 0");
+        return;
+    }
+    console.log(numA / numB);
+}
+
+add("102", "60");
+subtract("50", 30);
+multiply(2, "7");
+divide("64", "5");
